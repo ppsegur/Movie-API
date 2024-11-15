3d21942c09920c40d60c8fae65ca9bdb70d68691
Pablo <camara.gapab23@triana.salesianos.edu>
2024-11-15 12:02:13 +0100

    Creados componente, servicio e interfaz

---
+++ src/app/components/series-list/series-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-series-list',
+  templateUrl: './series-list.component.html',
+  styleUrl: './series-list.component.css'
+})
+export class SeriesListComponent {
+
+}
