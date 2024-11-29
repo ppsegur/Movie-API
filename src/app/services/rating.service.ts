97556b82d492544b648af8387b5cdb5615822634
Pablo <camara.gapab23@triana.salesianos.edu>
2024-11-29 13:29:31 +0100

    Cambios visuales del rating en series-list y para film-detail se puede añadir valoración y guardar en listado de valorados.

--- src/app/services/rating.service.ts
+++ src/app/services/rating.service.ts
@@ -4,6 +4,7 @@ import { FilmsService } from './films.service';
 import { Observable } from 'rxjs';
 import { Series } from '../../models/series.model';
 import { SeriesListComponent } from '../components/series-list/series-list.component';
+import { Films } from '../models/films.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -13,30 +14,26 @@ export class RatingService {
   API_KEY = '05e17ea68b0a29c92de23f76cc1cff22';
   API_URL = 'https://api.themoviedb.org/3';
 
-  listadoSeriesValoradas: any[] = [];
+  listadoValorados: Observable<Films>[] = [];
 
-  constructor(private http: HttpClient, private seriesList: SeriesListComponent) { }
+  constructor(private http: HttpClient, private filmsService: FilmsService) { }
 
     rateSeries(id:number, rating: number): Observable<any> {
       const url = `${this.API_URL}/movie/${id}/rating?api_key=${this.API_KEY}&session_id=${localStorage.getItem('session_id')}`;
-      return this.http.post(url, {value: rating});
 
       //Guardar la serie que se ha valorado en listadoSeriesValoradas
-      this.guardarSeriesValoradas(id);
+      this.guardarValorados(id);
+
+      return this.http.post(url, {value: rating});
+
     }
 
-    /*guardarSeriesValoradas(series: Series) {
-      this.listadoSeriesValoradas.push(series);
-    }*/
-
     //Guardar la serie que se ha valorado en listadoSeriesValoradas
-    guardarSeriesValoradas(id: number) {
-      for(let i = 0; i < this.seriesList.listadoSeries.length; i++) {
-        if(this.seriesList.listadoSeries[i].id == id) {
-          this.listadoSeriesValoradas.push(this.seriesList.listadoSeries[i]);
-        }
+    guardarValorados(id: number) {
+      this.listadoValorados.push(this.filmsService.getFilmById(id));
+      for(let i = 0; i < this.listadoValorados.length; i++) {
+        console.log(this.listadoValorados[i]);
       }
 
-      //this.listadoSeriesValoradas.push(ratedObject);
     }
 }
